da49e8451b1d14e065929c71d7c096b6e8dcf3de
rahil-samnani <rahilsamnani456@gmail.com>
2025-01-05 23:29:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,11 @@ const app = express()
 const port = 8080
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+ origin : ["https://deploy-mern-1whq.vercel.app"],
+ methods : ["POST","GET"],
+ credentials : true
+}))
 
 //Available Routes
 app.use('/api/auth' , require("./routes/auth"))
