423a4a3b4172750c1c81492aa215d23aac28bd83
rahil-samnani <rahilsamnani456@gmail.com>
2025-01-05 23:52:43 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -15,7 +15,7 @@ export default function Signup(props) {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        const response = await fetch("http://localhost:8080/api/auth/createuser", {
+        const response = await fetch("https://cloudquill.vercel.app/api/auth/createuser", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
