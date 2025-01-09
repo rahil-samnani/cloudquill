0b3e8064a5ff1ed506b15ba06a9cfa329fefbfba
rahil-samnani <rahilsamnani456@gmail.com>
2025-01-09 18:09:38 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,16 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import logo from '../images/logo.png'
+import NoteContext from '../context/notes/noteContext'
 
 
 export default function Signup(props) {
 
     const navigate = useNavigate()
+
+    const context = useContext(NoteContext)
+    //const { mode } = context
+    const mode = 'dark'
 
     const [credential, setCredential] = useState({ name: "", email: "", password: "" })
 
@@ -34,45 +39,45 @@ export default function Signup(props) {
     }
 
     return (
-        <div className='d-flex justify-content-center align-items-center' style={{ height: "100vh", width: "100%", backgroundColor: "f0f1f2" }}>
-            <div className=" Lflex-r Lcontainer border border-3 rounded">
+        <div className='d-flex justify-content-center align-items-center' style={{ height: "100vh", width: "100%", backgroundColor: `${mode==="light"?"#F0F1F2":"#2f2f2f"}` }}>
+            <div className=" Lflex-r Lcontainer" style={{border: `${mode==="light"?"2px solid #2f2f2f":"2px solid #f0f1f2"}` , borderRadius: "10px"}}>
                 <div className="Lflex-r Llogin-wrapper">
-                    <div className="Llogin-text">
+                    <div className="Llogin-text" style={{backgroundColor: `${mode==="light"?"#F0F1F2":"#2f2f2f"}`}}>
                         <div className="Llogo">
                             <span><img src={logo} alt="cloudquill" style={{ width: "50px", height: "50px" }} /></span>
-                            <span>CloudQuill</span>
+                            <span className={`text-${mode==='light'?'dark':'light'}`}>CloudQuill</span>
                         </div>
-                        <h1>Sign Up</h1>
-                        <p>It's not long before you embark on this journey! </p>
+                        <h1 className={`text-${mode==='light'?'dark':'light'}`}>Sign Up</h1>
+                        <p className={`text-${mode==='light'?'dark':'light'}`}>It's not long before you embark on this journey! </p>
 
                         <form className=" Lflex-c" onSubmit={handleSubmit}>
-                            <div className="Linput-box">
-                                <span className="Llabel">Name</span>
-                                <div className=" Lflex-r Linput">
-                                    <input minLength={3} required onChange={handleChange} value={credential.name} name='name' className='LLinput' type="text" placeholder="eg. Rahil" />
-                                    <i className="fa-solid fa-user"></i>
+                            <div className="Linput-box" >
+                                <span className={`Llabel text-${mode==='light'?'dark':'light'}`}>Name</span>
+                                <div className=" Lflex-r Linput" style={{backgroundColor: `${mode==="light"?"#F0F1F2":"#2f2f2f"}` }}>
+                                    <input minLength={3} required onChange={handleChange} value={credential.name} name='name' className='LLinput' type="text" placeholder="eg. Rahil" style={{backgroundColor: `${mode==="light"?"#F0F1F2":"#2f2f2f"}`, color : `${mode==='light'?"black":"#3DC4E2"}` }} />
+                                    <i style={{color : `${mode==='light'?"black":"#3DC4E2"}`}} className="fa-solid fa-user"></i>
                                 </div>
                             </div>
 
                             <div className="Linput-box">
-                                <span className="Llabel">E-mail</span>
-                                <div className=" Lflex-r Linput">
-                                    <input required onChange={handleChange} value={credential.email} name='email' className='LLinput' type="text" placeholder="name@abc.com" />
-                                    <i className="fas fa-at"></i>
+                                <span className={`Llabel text-${mode==='light'?'dark':'light'}`}>E-mail</span>
+                                <div className=" Lflex-r Linput" style={{backgroundColor: `${mode==="light"?"#F0F1F2":"#2f2f2f"}` }}>
+                                    <input required onChange={handleChange} value={credential.email} name='email' className='LLinput' type="text" placeholder="name@abc.com" style={{backgroundColor: `${mode==="light"?"#F0F1F2":"#2f2f2f"}`, color : `${mode==='light'?"black":"#3DC4E2"}` }}/>
+                                    <i style={{color : `${mode==='light'?"black":"#3DC4E2"}`}} className="fas fa-at"></i>
                                 </div>
                             </div>
 
                             <div className="Linput-box">
-                                <span className="Llabel">Password</span>
-                                <div className="Lflex-r Linput">
-                                    <input minLength={5} required onChange={handleChange} value={credential.password} name='password' className='LLinput' type="password" placeholder="8+ (a, A, 1, #)" />
-                                    <i className="fas fa-lock"></i>
+                                <span cclassName={`Llabel text-${mode==='light'?'dark':'light'}`}>Password</span>
+                                <div className="Lflex-r Linput" style={{backgroundColor: `${mode==="light"?"#F0F1F2":"#2f2f2f"}` }}>
+                                    <input minLength={5} required onChange={handleChange} value={credential.password} name='password' className='LLinput' type="password" placeholder="8+ (a, A, 1, #)" style={{backgroundColor: `${mode==="light"?"#F0F1F2":"#2f2f2f"}`, color : `${mode==='light'?"black":"#3DC4E2"}` }}/>
+                                    <i style={{color : `${mode==='light'?"black":"#3DC4E2"}`}} className="fas fa-lock"></i>
                                 </div>
                             </div>
 
                             <input className="btns" type="submit" value="Create an Account" />
                             <span className="Lextra-line">
-                                <span>Already have an account?</span>
+                                <span className={`text-${mode==='light'?'dark':'light'}`}>Already have an account?</span>
                                 <Link to="/login">Login</Link>
                             </span>
                         </form>
